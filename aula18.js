2b1f0c2358ec6e645b3110e2978599f397267583
henrique_Pedrozo <pedrozo140@gmail.com>
2025-01-31 10:06:13 -0300

    Aula 18

---
+++ aula18.js
@@ -0,0 +1,15 @@
+let n = 0
+
+//Esse já precisa de reconhecimento true para começar, se false, so pula tudo
+while(n < 10){
+    console.log("Henrique ama Giulia")
+    n++
+}
+n = 0
+
+
+// Esse pode começar sem a necessidade de ser true ou falso, ja que esse reconhecimento é feito so no final
+do {
+    console.log("Giulinha linda")
+    n++
+}while(n < 10)
