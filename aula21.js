5e262e950930f501fd8d4028e35be8aa04d3464f
henrique_Pedrozo <pedrozo140@gmail.com>
2025-01-31 12:29:55 -0300

    aula 21

--- aula21.js
+++ aula21.js
@@ -1,6 +1,8 @@
-function canal(n1, n2){
-    let resut = n1+n2
+function canal(){
+    let n1 = 10
+    let n2 = 12
+    let resut = n1*n2
     return resut
 }
 
-console.log(canal(1,3))
+console.log(canal())
